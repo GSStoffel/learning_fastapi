bd7c0b6a30dde3792eba8616acea18452b26c999
Gabriel Stoffel <gsperbstoffel@gmail.com>
2025-07-16 20:56:35 -0300

    Alterando a forma em que é passado as credenciais usando OAuth2PasswordRequestForm

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from app import schemas, models, oauth2
@@ -9,8 +10,8 @@ router = APIRouter(tags=['Authentication'])
 
 
 @router.post('/login')
-def login(user_credential: schemas.UserLogin, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.email == user_credential.email).first()
+def login(user_credential: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.email == user_credential.username).first()
 
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Cretentials")
