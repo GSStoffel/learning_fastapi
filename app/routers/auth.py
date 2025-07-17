a2e4a87060d730a56e4d527f6bfff9c504f0f391
Gabriel Stoffel <gsperbstoffel@gmail.com>
2025-07-16 21:47:43 -0300

    Adicionando response_model ao login

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -2,14 +2,14 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
-from app import models, oauth2
+from app import models, oauth2, schemas
 from app.database import get_db
 from app.utils import verify
 
 router = APIRouter(tags=['Authentication'])
 
 
-@router.post('/login')
+@router.post('/login', response_model=schemas.Token)
 def login(user_credential: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.email == user_credential.username).first()
 
