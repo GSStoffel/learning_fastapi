02ac7456355e6314abad7918ae31a0380ec554c8
Gabriel Stoffel <gsperbstoffel@gmail.com>
2025-07-16 21:24:03 -0300

    Alteração do status HTTP retornado em erro de credenciais

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
-from app import schemas, models, oauth2
+from app import models, oauth2
 from app.database import get_db
 from app.utils import verify
 
@@ -14,11 +14,11 @@ def login(user_credential: OAuth2PasswordRequestForm = Depends(), db: Session = 
     user = db.query(models.User).filter(models.User.email == user_credential.username).first()
 
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Cretentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Cretentials")
 
     if not verify(plain_password=user_credential.password, hash_password=user.password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Cretentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Cretentials")
 
-    access_token = oauth2.create_access_token(data = {"user_id": user.id})
+    access_token = oauth2.create_access_token(data={"user_id": user.id})
 
     return {"access_token": access_token, "token_type": "bearer"}
