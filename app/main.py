6923c539e91b673a612b41289bd4bea88337e607
Gabriel Stoffel <gsperbstoffel@gmail.com>
2025-07-18 18:21:45 -0300

    Refatorando código

--- app/main.py
+++ app/main.py
@@ -1,6 +1,4 @@
-import psycopg2
 from fastapi import FastAPI
-from psycopg2.extras import RealDictCursor
 
 from app import models
 from app.database import engine
@@ -9,21 +7,6 @@ from app.routers import post, user, auth
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-try:
-    conn = psycopg2.connect(
-        host='localhost',
-        database='fastapi',
-        user='postgres',
-        password='admin',
-        cursor_factory=RealDictCursor
-    )
-    cursor = conn.cursor()
-    print("Connected to PostgreSQL")
-
-except Exception as error:
-    print("Connection to PostgreSQL failed")
-    print("Error: ", error)
 
 app.include_router(post.router)
 app.include_router(user.router)
