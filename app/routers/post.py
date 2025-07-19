4ed618e415b688684f1948d7ffd2d667193f5a71
Gabriel Stoffel <gsperbstoffel@gmail.com>
2025-07-19 00:04:01 -0300

    Post
    Refatoração

--- app/routers/post.py
+++ app/routers/post.py
@@ -44,18 +44,6 @@ def get_posts(
     return posts
 
 
-@router.post("", status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
-def create_post(post: schemas.PostCreate,
-                db: Session = Depends(get_db),
-                current_user: UserResponse = Depends(get_current_user)):
-    new_post = models.Post(owner_id=current_user.id, **post.model_dump())
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
-
-    return new_post
-
-
 @router.get("/my_posts", response_model=List[schemas.PostResponse])
 def get_my_posts(db: Session = Depends(get_db), current_user: schemas.UserResponse = Depends(get_current_user)):
     posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
@@ -72,6 +60,26 @@ def get_latest_post(db: Session = Depends(get_db), current_user: UserResponse = 
     if not post:
         return Response(status_code=status.HTTP_204_NO_CONTENT)
     return post
+
+
+@router.get("/{id}", response_model=schemas.PostResponse)
+def get_post(id: int, db: Session = Depends(get_db), current_user: UserResponse = Depends(get_current_user)):
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post {id} not found")
+    return post
+
+
+@router.post("", status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
+def create_post(post: schemas.PostCreate,
+                db: Session = Depends(get_db),
+                current_user: UserResponse = Depends(get_current_user)):
+    new_post = models.Post(owner_id=current_user.id, **post.model_dump())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+
+    return new_post
 
 
 @router.put("/{id}", response_model=schemas.PostResponse)
@@ -106,11 +114,3 @@ def delete_post(id: int, db: Session = Depends(get_db), current_user: UserRespon
     db.commit()
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-
-@router.get("/{id}", response_model=schemas.PostResponse)
-def get_post(id: int, db: Session = Depends(get_db), current_user: UserResponse = Depends(get_current_user)):
-    post = db.query(models.Post).filter(models.Post.id == id).first()
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post {id} not found")
-    return post
